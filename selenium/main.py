0d6c8762f2facaa8d042d31700241c84a248764c
Paulo Adeva <paulo.adeva@gmail.com>
2023-03-13 09:49:46 +1100

    done

--- selenium/main.py
+++ selenium/main.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
 options = Options()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
@@ -14,42 +15,26 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
 driver.get("http://www.uts.edu.au/")
 driver.maximize_window()
 
-# links = driver.find_elements("xpath", "//a[@href]")
-# for link in links:
-#     if "Engineering" in link.get_attribute("innerHTML"):
-#         link.click()
-#         break
+try:
+    studyLink = driver.find_element("xpath", "//span[@data-href='/study']")
+    studyLink.click()
 
-studyLink = driver.find_element("xpath", "//span[@data-href='/study']")
-studyLink.click()
+    inputElement = driver.find_element(By.ID, "edit-search")
+    inputElement.send_keys('C09070')
+    inputElement.send_keys(Keys.ENTER)
 
+    courseLink = driver.find_elements("xpath", "//a[@href]")
+    for link in courseLink:
+        if "Bachelor of Engineering (Honours) Bachelor of Business" in link.get_attribute("innerHTML"):
+            link.click()
+            break
 
-# links = driver.find_elements("xpath", "//a[@href]")
-# for link in links:
-#     print(link.get_attribute("innerHTML"))
-#     # if "Engineering" in link.get_attribute("innerHTML"):
-#     #     link.click()
-#     #     break
+    time.sleep(3)
 
-inputElement = driver.find_element(By.ID, "edit-search")
-inputElement.send_keys('C09070')
-inputElement.send_keys(Keys.ENTER)
+    atar = driver.find_element("xpath", "//div[@class='sidebar__info sidebar--info-atar']//p")
 
-courseLink = driver.find_elements("xpath", "//a[@href]")
-for link in courseLink:
-    if "Bachelor of Engineering (Honours) Bachelor of Business" in link.get_attribute("innerHTML"):
-        link.click()
-        break
+    atarNum = atar.get_attribute("innerHTML")
+    print(atarNum.strip())
 
-time.sleep(3)
-
-atar = driver.find_element("xpath", "//div[@class='sidebar__info sidebar--info-atar']")
-
-# for i in atar:
-#     tagName = atar[i].tag_name
-#     if tagName.startswith("p"):
-#         print(tagName.get_attribute("innerHTML"))
-#         break
-
-
-print(atar.get_attribute("innerHTML"))
+finally:
+    driver.quit()
